c801263ec8dc10f80d9c305baa97df147f4bb3e4
akczay <smt.akcay@gmail.com>
2018-08-09 21:49:30 +0100

    Added `mnist2 dataloader

--- experiments/run_mnist.sh
+++ experiments/run_mnist.sh
@@ -8,7 +8,7 @@ do
     for i in {0..9}
     do
         echo "Running mnist_$i"
-        python train.py --dataset mnist --isize 32 --nc 1 --niter 15 --anomaly_class $i --manualseed $m
+        python train.py --dataset mnist --isize 32 --nc 1 --niter 15 --anomaly_class $i --manualseed $m --display
     done
 done
 exit 0
--- lib/data.py
+++ lib/data.py
@@ -106,6 +106,42 @@ def load_data(opt):
                                                      drop_last=drop_last_batch[x]) for x in splits}
         return dataloader
 
+    elif opt.dataset in ['mnist2']:
+        opt.anomaly_class = int(opt.anomaly_class)
+
+        splits = ['train', 'test']
+        drop_last_batch = {'train': True, 'test': False}
+        shuffle = {'train': True, 'test': True}
+
+        transform = transforms.Compose(
+            [
+                transforms.Scale(opt.isize),
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ]
+        )
+
+        dataset = {}
+        dataset['train'] = MNIST(root='./data', train=True, download=True, transform=transform)
+        dataset['test'] = MNIST(root='./data', train=False, download=True, transform=transform)
+
+        dataset['train'].train_data, dataset['train'].train_labels, \
+        dataset['test'].test_data, dataset['test'].test_labels = get_mnist2_anomaly_dataset(
+            trn_img=dataset['train'].train_data,
+            trn_lbl=dataset['train'].train_labels,
+            tst_img=dataset['test'].test_data,
+            tst_lbl=dataset['test'].test_labels,
+            nrm_cls_idx=opt.anomaly_class,
+            proportion=0.1
+        )
+
+        dataloader = {x: torch.utils.data.DataLoader(dataset=dataset[x],
+                                                     batch_size=opt.batchsize,
+                                                     shuffle=shuffle[x],
+                                                     num_workers=int(opt.workers),
+                                                     drop_last=drop_last_batch[x]) for x in splits}
+        return dataloader
+
     else:
         splits = ['train', 'test']
         drop_last_batch = {'train': True, 'test': False}
@@ -275,5 +311,63 @@ def get_mnist_anomaly_dataset(trn_img, trn_lbl, tst_img, tst_lbl, abn_cls_idx=0,
     new_trn_lbl = nrm_trn_lbl.clone()
     new_tst_img = torch.cat((nrm_tst_img, abn_trn_img, abn_tst_img), dim=0)
     new_tst_lbl = torch.cat((nrm_tst_lbl, abn_trn_lbl, abn_tst_lbl), dim=0)
+
+    return new_trn_img, new_trn_lbl, new_tst_img, new_tst_lbl
+
+##
+def get_mnist2_anomaly_dataset(trn_img, trn_lbl, tst_img, tst_lbl, nrm_cls_idx=0, proportion=0.1):
+    """ Create mnist 2 anomaly dataset.
+
+    Arguments:
+        trn_img {np.array} -- Training images
+        trn_lbl {np.array} -- Training labels
+        tst_img {np.array} -- Test     images
+        tst_lbl {np.array} -- Test     labels
+
+    Keyword Arguments:
+        nrm_cls_idx {int} -- Anomalous class index (default: {0})
+
+    Returns:
+        [tensor] -- New training-test images and labels.
+    """
+    # --
+    # Find normal abnormal indexes.
+    # TODO: PyTorch v0.4 has torch.where function
+    nrm_trn_idx = torch.from_numpy(np.where(trn_lbl.numpy() == nrm_cls_idx)[0])
+    abn_trn_idx = torch.from_numpy(np.where(trn_lbl.numpy() != nrm_cls_idx)[0])
+    nrm_tst_idx = torch.from_numpy(np.where(tst_lbl.numpy() == nrm_cls_idx)[0])
+    abn_tst_idx = torch.from_numpy(np.where(tst_lbl.numpy() != nrm_cls_idx)[0])
+
+    # Get n percent of the abnormal samples.
+    abn_tst_idx = abn_tst_idx[torch.randperm(len(abn_tst_idx))]
+    abn_tst_idx = abn_tst_idx[:int(len(abn_tst_idx) * proportion)]
+
+
+    # --
+    # Find normal and abnormal images
+    nrm_trn_img = trn_img[nrm_trn_idx]    # Normal training images
+    abn_trn_img = trn_img[abn_trn_idx]    # Abnormal training images.
+    nrm_tst_img = tst_img[nrm_tst_idx]    # Normal training images
+    abn_tst_img = tst_img[abn_tst_idx]    # Abnormal training images.
+
+    # --
+    # Find normal and abnormal labels.
+    nrm_trn_lbl = trn_lbl[nrm_trn_idx]    # Normal training labels
+    abn_trn_lbl = trn_lbl[abn_trn_idx]    # Abnormal training labels.
+    nrm_tst_lbl = tst_lbl[nrm_tst_idx]    # Normal training labels
+    abn_tst_lbl = tst_lbl[abn_tst_idx]    # Abnormal training labels.
+
+    # --
+    # Assign labels to normal (0) and abnormals (1)
+    nrm_trn_lbl[:] = 0
+    nrm_tst_lbl[:] = 0
+    abn_trn_lbl[:] = 1
+    abn_tst_lbl[:] = 1
+
+    # Create new anomaly dataset based on the following data structure:
+    new_trn_img = nrm_trn_img.clone()
+    new_trn_lbl = nrm_trn_lbl.clone()
+    new_tst_img = torch.cat((nrm_tst_img, abn_tst_img), dim=0)
+    new_tst_lbl = torch.cat((nrm_tst_lbl, abn_tst_lbl), dim=0)
 
     return new_trn_img, new_trn_lbl, new_tst_img, new_tst_lbl
