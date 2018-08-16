eae43364a22ba5f19fc50d6742bf5cbf6ca7d641
akczay <smt.akcay@gmail.com>
2018-08-16 21:26:57 +0100

    Added metric flag to the modle

--- lib/model.py
+++ lib/model.py
@@ -351,7 +351,7 @@ class Ganomaly:
             # Scale error vector between [0, 1]
             self.an_scores = (self.an_scores - torch.min(self.an_scores)) / (torch.max(self.an_scores) - torch.min(self.an_scores))
             # auc, eer = roc(self.gt_labels, self.an_scores)
-            auc = evaluate(self.gt_labels, self.an_scores)
+            auc = evaluate(self.gt_labels, self.an_scores, metric=self.opt.metric)
             performance = OrderedDict([('Avg Run Time (ms/batch)', self.times), ('AUC', auc)])
 
             if self.opt.display_id > 0 and self.opt.phase == 'test':
