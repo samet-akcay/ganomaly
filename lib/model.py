c3cb66ea7197d42ca2a6ff7ea4a4cf9daef6e889
Samet Akcay <samet-akcay@github.com>
2018-09-29 09:56:33 +0100

    Added self.device=cpu option to be able to run on cpu

--- lib/model.py
+++ lib/model.py
@@ -40,7 +40,7 @@ class Ganomaly:
         self.dataloader = dataloader
         self.trn_dir = os.path.join(self.opt.outf, self.opt.name, 'train')
         self.tst_dir = os.path.join(self.opt.outf, self.opt.name, 'test')
-        self.device = torch.device("cuda:0" if self.opt.gpu_ids!=-1 else "cpu")
+        self.device = torch.device("cuda:0" if self.opt.device != 'cpu' else "cpu")
 
         # -- Discriminator attributes.
         self.out_d_real = None
