7c2f75597467bebf4114a1b57cdc8f3d8a38d474
akczay <smt.akcay@gmail.com>
2018-10-02 09:42:31 +0100

    Use Resize instead of Scale

--- lib/data.py
+++ lib/data.py
@@ -40,7 +40,7 @@ def load_data(opt):
 
         transform = transforms.Compose(
             [
-                transforms.Scale(opt.isize),
+                transforms.Resize(opt.isize),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
             ]
